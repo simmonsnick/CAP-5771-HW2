066e885859cb81c2a88ba309cd7c3fc9a9fa965d
Nicholas <93218182+simmonsnick@users.noreply.github.com>
2024-02-15 20:59:40 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -1,12 +1,10 @@
- Answer found in Q5 in Question Bank 1 (Tan et al, 2nd ed)
+#Answer found in Q5 in Question Bank 1 (Tan et al, 2nd ed)
 #import student_code_with_answers.utils as u
 import utils as u
-
 
 # Example of how to specify a binary with the structure:
 # See the file INSTRUCTIONS.md
 # ----------------------------------------------------------------------
-
 
 def question1():
     """
@@ -22,16 +20,16 @@ def question1():
     level2_left = {}
     level2_right = {}
   
-    level1["smoking"] = 1         #the average of yes and no entrophy
+    level1["smoking"] = 1           #the average of yes and no entrophy
     level1["smoking_info_gain"] = 0.3
 
-    level1["cough"] = -1           #the average of yes and no entrophy
+    level1["cough"] = -1            #the average of yes and no entrophy
     level1["cough_info_gain"] = 0.2
 
     level1["radon"] = -1            #the average of yes and no entrophy
     level1["radon_info_gain"] = 0.2
 
-    level1["weight_loss"] = -1       #the average of yes and no entrophy
+    level1["weight_loss"] = -1      #the average of yes and no entrophy
     level1["weight_loss_info_gain"] = 0.1
 
     level2_left["smoking"] = -1
@@ -64,15 +62,40 @@ def question1():
 
     # Fill up `construct_tree``  
     # tree, training_error = construct_tree()
+    import utils as u
     tree = u.BinaryTree("smoking")  # MUST STILL CREATE THE TREE *****
     A = tree.insert_left("radon")
     B = tree.insert_right("cough")
+    A.insert_left("y")
+    A.insert_right("n")
+    B.insert_left("y")
+    B.insert_right("n")
     answer["tree"] = tree  # use the Tree structure
     # answer["training_error"] = training_error
     answer["training_error"] = 0  
 
     return answer
 
+#Q1 Calculations 
+#Entire Dataset
+import math
+entropy = -(5/10) * math.log2(5/10) - (5/10) * math.log2(5/10) 
+#Smoking
+split = -(4/5) * math.log2(4/5) - (1/5) * math.log2(1/5)
+split = -(1/5) * math.log2(1/5) - (4/5) * math.log(4/5)
+gain = entropy - split
+print(gain)
+
+#Chronic Cough
+split = -(4/5) * math.log2(4/5) - (3/5) * math.log2(3/5)
+split = -(1/5) * math.log2(1/5) - (2/5) * math.log2(2/5)
+gain  = entropy - split
+print(gain)
+#Weight loss
+split = -(3/5) * math.log2(3/5) - (2/5) * math.log2(2/5)
+split = -(2/5) * math.log2(2/5) - (3/5) * math.log2(3/5)
+gain = entropy - split
+print(gain)
 
 # ----------------------------------------------------------------------
 
@@ -81,41 +104,60 @@ def question2():
     answer = {}
 
     # Answers are floats
-    answer["(a) entropy_entire_data"] = 1.422
+    answer["(a) entropy_entire_data"] = 1.425
     # Infogain
-    answer["(b) x <= 0.2"] = 1.266
-    answer["(b) x <= 0.7"] = 0.926
-    answer["(b) y <= 0.6"] = 0.767
+    answer["(b) x <= 0.2"] = 0.177
+    answer["(b) x <= 0.7"] = 1.046
+    answer["(b) y <= 0.6"] = 1.783 
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
     answer["(c) attribute"] = "y = 0.6"  
 
     # Use the Binary Tree structure to construct the tree
     # Answer is an instance of BinaryTree
+    import utils as u
     tree = u.BinaryTree("y <= 0.6")
     answer["(d) full decision tree"] = tree
-     tree = u.BinaryTree("y <= 0.6")
-    tree.insert_left("Yes")
-    tree.insert_right("No")
-    b = tree.insert_right("x <= 0.7")
-    b.insert_left("A")
-    b.insert_right("B")
-    tree = u.BinaryTree("x <= 0.7")
-    tree.insert_left("Yes")
-    tree.insert_right("No")
-    b.insert_left("A")
-    b.insert_right("C")
-    tree = u.BinaryTree("x <= 0.2")
-    tree.insert_left("Yes")
-    tree.insert_right("No")
-    b.insert_left("B")
-    b.insert_right("C")
-    tree = u.BinaryTree("y <= 0.8")
-    tree.insert_left("Yes")
-    tree.insert_right("No")
-    b.insert_left("A")
-    b.insert_right("B")
+    tree = u.BinaryTree("y <= 0.6")
+    #L2 Left
+    A = tree.insert_left("x <= 0.7")
+    #L2 Right
+    B = tree.insert_right("x <= 0.2")
+    A.insert_left("y")
+    A.insert_right("n")
+    B.insert_left("y")
+    B.insert_right("n")
     return answer
+
+#Q2 Calculations 
+#Entire Dataset
+#Class A = 0.41, Class B = 0.46, Class C = 0.13
+import math
+entropy = -(0.41) * math.log2(0.41) - (0.46) * math.log2(0.46) - (0.13) * math.log2(0.13) 
+print("total entropy")
+print(entropy)
+# x <= 0.2
+entropy_r1 = -(0.16/0.2) * math.log2(0.16/0.2) - (0.04/0.2) * math.log2(0.04/0.2)
+print(entropy_r1)
+entropy_r2 =  -(0.09/0.8) * math.log2(0.09/0.8) - (0.3/0.8) * math.log2(0.3/0.8) - (0.41/0.8) * math.log2(0.41/0.8)
+print(entropy_r2)
+print('values for x<=0.2')
+print(entropy - 0.2*entropy_r1 - 0.8*entropy_r2)
+# x <= 0.7
+entropy_r3 = -(0.2/0.7) * math.log2(0.2/0.7) + (0.04/0.7) * math.log2(0.04/0.7) 
+print(entropy_r3)
+entropy_r4 = -(0.09/0.3) * math.log2(0.09/0.3) + (0.12/0.3) * math.log2(0.12/0.3) + (0.41/0.3) * math.log2(0.41/0.3)
+print(entropy_r4)
+print('values= for x<=0.7')
+print(entropy - 0.7*entropy_r3 - entropy_r4*0.3)
+# y <= 0.6
+entropy_r5 = -(0.32/0.4) * math.log2(0.32/0.4) + (0.04/0.4) * math.log2(0.04/0.4) + (0.04/0.4) * math.log2(0.04/0.4)
+print(entropy_r1)
+entropy_r6 = -(0.09/0.6) * math.log2(0.09/0.6) + (0.42/0.6) * math.log2(0.42/0.6) + (0.41/0.6) * math.log2(0.41/0.6)
+print(entropy_r2)
+print('values for y<=0.6')
+print(entropy - 0.4*entropy_r5 - entropy_r6*0.6)
+
 # ----------------------------------------------------------------------
 
 def question3():
@@ -135,6 +177,25 @@ def question3():
 
     return answer
 
+#Calculations
+#Entire Dataset
+gini = 1 - (10/20)**2 - (10/20)**2 
+#Part b
+gini = 1 - (0/1)**2 - (1/1)**2
+#Part c
+gini_m = 1 - (6/10)**2 - (4/10)**2
+gini_f = 1 - (4/10)**2 - (6/10)**2
+
+#Part d
+gini_f = 1 - (1/4)**2 - (3/4)**2
+gini_s = 1 - (0/8)**2 - (8/8)**2
+gini_l = 1 - (1/8)**2 - (7/8)**2
+
+#Part e
+gini_s = 1 - (3/5)**2 - (2/5)**2
+gini_m = 1 - (3/7)**2 - (4/7)**2
+gini_lg = 1 - (2/4)**2 - (2/4)**2
+gini_xl = 1 - (2/4)**2 - (2/4)**2
 
 # ----------------------------------------------------------------------
 # Answers in th form [str1, str2, str3]
@@ -224,28 +285,22 @@ def question6():
     # value of the form "z <= float" where "z" is "x" or "y"
     #  and "float" is a floating point number (notice: <=)
     # The value could also be "A" or "B" if it is a leaf
-    answer["a, level 1"] = "x <= c"
-    answer["a, level 2, right"] ="y <= c"
-    answer["a, level 2, left"] = "x <= c"
-    answer["a, level 3, left"] = "y <= c2"
-    answer["a, level 3, right"] = "x > c2"
-
+    answer["a, level 1"] = "x <= 0.5"
+    answer["a, level 2, right"] ="y <= 0.4"
+    answer["a, level 2, left"] = "x <= A"
+    answer["a, level 3, left"] = "y <= B"
+    answer["a, level 3, right"] = "x <= 0.2"
     # run each datum through the tree. Count the number of errors and divide by number of samples. .
     # Since we have areas: calculate the area that is misclassified (total area is unity)
     # float between 0 and 1
-    answer["b, expected error"] = 1.427
-
+    answer["b, expected error"] = 0.58
     # Use u.BinaryTree to define the tree. Create your tree.
     # Replace "root node" by the proper node of the form "z <= float"
-    tree = u.BinaryTree("x <= c")
-    A = tree.insert_left("x <= c")
-    B = tree.insert_right("y <= c")
-    A.insert_left("A")
-    A.insert_right("B")
-    B.insert_left("A")
-    B.insert_right("A")
+    import utils as u
+    tree = u.BinaryTree("x <= 0.5")
+    A = tree.insert_left("y <= 0.4")
+    B = tree.insert_right("x <= A")
     answer["c, tree"] = tree
-
     return answer
 
 
@@ -269,7 +324,6 @@ def question7():
     answer["f, which attrib"] = "neither"
 
     return answer
-
 
 # ----------------------------------------------------------------------
 
